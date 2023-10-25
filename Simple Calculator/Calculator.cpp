a2456eecc4ff929fea579c073c23e21650d9e077
Muzamil Zaman Khan <95095602+MuzamilDev@users.noreply.github.com>
2023-10-25 18:03:20 +0500

    Add files via upload

---
+++ Simple Calculator/Calculator.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+
+
+{
+float num1 , num2;
+char op;
+
+cout<<endl;
+cout<<"Enter first Number "<<endl;
+cin>>num1;
+
+cout<<"What Do You Want: + , - , * , / "<<endl;
+cin>>op;
+
+cout<<"Enter Second Number "<<endl;
+cin>>num2;
+
+switch(op){
+case '+':
+cout<<num1<<"+"<<num2<<"="<<num1+num2<<endl;
+break;
+case '-':
+cout<<num1<<"-"<<num2<<"="<<num1-num2<<endl;
+break;
+case'*':
+cout<<num1<<"*"<<num2<<"="<<num1*num2<<endl;
+break;
+case '/':
+cout<<num1<<"/"<<num2<<"="<<num1/num2<<endl;
+break;
+
+return 0;
+}
+
+}
